018900489e47d2e6403923e7e7f66701af0b1891
okwell-me <46261997+okwell-me@users.noreply.github.com>
2021-07-02 01:37:18 +0300

    Deleted odd functions

--- listutils.cpp
+++ listutils.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 struct Node {
   int val;
   Node* next;
@@ -22,59 +24,12 @@ void pushNode(Node* head,Node* node) {
 } 
 
 
-// Написать функцию инициализации size элементов списка заданным значением val.
-void initList(Node* head, int size, int val)  {
-
-}
-
-// Написать функцию копирования элементов массива source в новый массив dest
-void copyMas(Node* source, Node* dest) {
-
-}
-  
-
-// Написать функцию сравнение списков source и dest. Если списки равны 0, если не равны -1
-int compareMas(Node* source, Node* dest) {
-  return 0;
-}
-
-
-// Написать функцию удаления со сдвигом влево n-го элемента списка.
-void deleteList(Node* head,int n) {
-
-}
-
-
-// Написать функцию сдвига значений вправо на n-элементов с заполнением крайних элементов нулями
-void rShiftList(Node* head,int n) {
-
-}
-
-
-
-// Написать функцию сдвига значений влево на n-элементов с заполнением крайних элементов нулями.
-void lShiftList(Node* head,int n) {
-
-}
-
-
-//Написать функцию сдвига значений влево на n-элементов с переносом вытесненных элементов в конец
-void lRoundShiftMas(Node* head,int n, int size) {
-
-}
-
-
 // Написать функцию вычисления среднего значения элементов списка.
 int averageList(Node* head, int size) {
+
   return 0;
 }
 
-
-
-// Написать функцию вычисления средне квадратического отклонения элементов списка.
-int skoFromList(Node* head, int size) {
-  return 0;
-}
 
 
 int testSkoFromList() {
@@ -82,12 +37,12 @@ int testSkoFromList() {
 }
 
 
-static void runTest(int (*testFunction)(),const std::string& testName)
+static void runTest(int (*testFunction)(),const string& testName)
 {
   if(testFunction()==0)
-    std::cout << "Test "<< testName << " - OK" << std::endl;
+    cout << "Test "<< testName << " - OK" << endl;
   else 
-    std::cout << "Test "<< testName << " - FAIL" << std::endl;
+    cout << "Test "<< testName << " - FAIL" << endl;
 }
 
 
